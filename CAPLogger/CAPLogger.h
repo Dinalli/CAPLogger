3c6c618b478221dab17f2ea63bae42ed5f77bcb1
Andrew Donnelly <Dinalli@hotmail.com>
2016-08-09 09:23:10 +0100

    Initial Commit

---
+++ CAPLogger/CAPLogger.h
@@ -0,0 +1,19 @@
+//
+//  CAPLogger.h
+//  CAPLogger
+//
+//  Created by Andrew Donnelly on 09/08/2016.
+//  Copyright © 2016 jlr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CAPLogger.
+FOUNDATION_EXPORT double CAPLoggerVersionNumber;
+
+//! Project version string for CAPLogger.
+FOUNDATION_EXPORT const unsigned char CAPLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAPLogger/PublicHeader.h>
+
+
