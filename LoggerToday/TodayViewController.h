5b52e0869cea1f94cc6daa312054d322f169582d
Andrew Donnelly <Dinalli@hotmail.com>
2016-08-15 10:50:49 +0100

    Added Today Logger

---
+++ LoggerToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  LoggerToday
+//
+//  Created by Andrew Donnelly on 15/08/2016.
+//  Copyright © 2016 jlr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
